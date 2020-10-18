fb89487ae32b55b4fafdec889b70136c8508ff41
Rishav <rishavchakraborty1998@gmail.com>
2020-10-19 02:55:12 +0530

    CityInfo:Fifth Commit--Added configuration file(appsettings.json) for both Production and Development Environment

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,13 +10,17 @@ namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private readonly IConfiguration _configuration;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
 
         public void Send(string subject, string message)
         {
             //send mail - output to debug window
-            Debug.WriteLine($"Mail from {_mailTo} to {_mailFrom}, with CloudMailService. ");
+            Debug.WriteLine($"Mail from {_configuration["mailSettings:mailFromAddress"]} to {_configuration["mailSettings:mailToAddress"]}, with LocalMailService. ");
             Debug.WriteLine($"Subject: {subject}");
             Debug.WriteLine($"Message: {message}");
         }
